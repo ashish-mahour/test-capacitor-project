0ad1b65f02bc7623fcba62f2d38188ebdd26b8c9
Ashish Kumar <ashish.kumar@tlcgroup.com>
2023-08-17 13:07:23 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'test-capacitor-project',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  },
+};
+
+export default config;
